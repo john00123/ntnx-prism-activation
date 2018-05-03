1d0b0c836444a152f60d0a95298bb2fc20326aaa
John Torres <john.torres.hurtado@gmail.com>
2018-05-02 20:30:30 -0700

    changes

---
+++ js/activate.js
@@ -0,0 +1,34 @@
+const activation=`
+  <h2>Product activation</h2>
+  <p>
+    In order to run this cluster you need to license it.
+  </br>Login to the licensing page in our support portal to upload the cluster summary file.
+    Follow the steps and generate the License file.
+  </p>
+  <div clas='two-col'>
+    <button class='secondary' style='margin-bottom:20px; margin-right:10px;'>
+      Download Cluster File
+    </button>
+    <button class='secondary' style='margin-bottom:20px;'>
+      Go to licensing Page
+    </button>
+  </div>
+  <hr>
+
+  <p>
+    Follow the steps to generate a Licensing File and upload it here to activate your cluster.
+  </p>
+  <div clas='two-col'>
+
+    <button class='primary' style='margin-bottom:20px;'>
+      Upload license
+    </button>
+  </div>
+  <hr>
+
+
+  <div class='two-col fw btns'>
+    <button class='secondary previous'>Previous</button>
+    <button class='primary accept' style='opacity:0.4'>Activate</button>
+  </div>
+`;
--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
-const usage=`
-  <h2>Usage Data</h2>
-  <p>
-    Pulse continiously monitors cluster and health periodically and sends data to Nutanix's cloud analytics engine. This information is anonymous and serves fro troubleshooting. Nutanix strongly recommends not to disable this featyre to improve your support experience in the evnet of a failure or critical system issue. If tyou sibale Pulse, Nutanix will not be able to proactively reach out to you in teh events of failures and the resolution time will increase significantly.
-  </p>
+const content = [eula, pulse, activation];
 
-  <h3><input type='radio' name='a' style='margin-right:10px;'> Don't share my data </h3>
-  <h3><input type='radio' name='a' style='margin-right:10px;'> Share support data </h3>
-  <h3><input type='radio' name='a' style='margin-right:10px;'> Share entity and support data (Enhance Cluster Monitoring)</h3>
-  <hr>
+let pageload = (index) => {
+  let page = index
+  $('.new-content').html(content[page])
+  $('.accept')  .click(()=>{ page += 1; pageload(page);})
+  $('.previous').click(()=>{ page -= 1; pageload(page);})
+  $('.expand').click(()=>{
+    $('.new-content hr').before(pulsehd);
+    $('.disable-pulse').hide();
+    $('.accept').show();
+    $('.expand').hide();
+  })
+  switch (page) {
+    case 0:
+      $(`aside h3`).removeClass();
+      $(`aside h3:eq(1)`).addClass('inactive');
+      $(`aside h3:eq(2)`).addClass('inactive');
+      break;
+    case 1:
+      $(`aside h3`).removeClass();
+      $(`aside h3:eq(0)`).addClass('completed');
+      $(`aside h3:eq(0)`).addClass('completed');
+      $(`aside h3:eq(2)`).addClass('inactive');
+      break;
+    case 2:
+      $(`aside h3`).removeClass();
+      $(`aside h3:eq(0)`).addClass('completed');
+      $(`aside h3:eq(1)`).addClass('completed');
+      break;
+    default:null
 
-  <div class='two-col fw btns'>
-    <button class='secondary'>Previous</button>
-    <button class='primary accept2'>  Accept</button>
-  </div>
-`;
-const activation=`
-  <h2>Product activation</h2>
-  <p>
-    In order to run this cluster you need to license it. This can be done by accesing this link and using this file to generate the license file. Upload the license file here.
-  </p>
-  <button class='secondary' style='margin-bottom:20px;'>Upload License File</button>
-  <hr>
-
-  <div class='two-col fw btns'>
-    <button class='secondary'>Previous</button>
-    <button class='primary accept2'>Finish</button>
-  </div>
-`;
-
-
+  }
+}
 
 $(document).ready(function() {
-
-  $('.accept').click(()=>{
-    $('.agreed, .signature, .eula').remove();
-    $('.new-content').html(usage);
-    $('aside h3:eq(0)').addClass('inactive');
-    $('aside h3:eq(1)').removeClass('inactive');
-    $('.accept2').unbind('click').bind('click',()=>{
-      $('.new-content').html(activation);
-    })
-  })
-
-
+  const index = 0
+  pageload(index)
 });
