11d5b1273d9d02573bab3d83115345338ac56ec2
John <john00123@users.noreply.github.com>
2018-05-07 23:48:57 -0700

    changes

---
+++ ntnx-prism-activation/js/activate.js
@@ -0,0 +1,25 @@
+const activation=`
+  <h2>Product activation</h2>
+  <p>
+    In order to run this cluster you need to license it.
+  </br>Login to the licensing page in our support portal to upload the cluster summary file provided.
+  </p>
+  <div clas='two-col'>
+    <a href='../img/initial.cluster' download><button class='secondary' style='margin-bottom:20px; margin-right:10px;'>
+      Download Cluster File
+    </button></a>
+  <a href='https://john00123.github.io/ntnx-CCU-portal/overview.html'>  <button class='secondary' style='margin-bottom:20px;'>
+      Licensing Page
+    </button></a>
+  </div>
+  <hr>
+
+  <p>
+    Follow the steps to generate a Licensing File and upload it here to activate your cluster.
+  </p>
+  <div clas='two-col'>
+    <input type="file" id='file'>
+  </div>
+
+  <a href='http://john00123.github.io/ntnx-CCU-portal/element.html'>  <button class='primary inactive'> Activate</button></a>
+`;
---
+++ ntnx-prism-activation/js/pulse.js
@@ -0,0 +1,30 @@
+const pulse=
+`
+  <h2>Pulse Data</h2>
+  <p style='white-space:pre-wrap'>Pulse continuously monitors cluster health and periodically sends machine data to Nutanix’s cloud based analytics engine. It automatically takes action when a problem has occurred or is about to occur.  <a href=’#’>Learn More</a>
+
+Nutanix strongly recommends not disabling this feature to improve your support experience in the event of a failure or critical system issue.
+
+Warning: If you disable Pulse, Nutanix will not be able to proactively reach out to you in the event of failures, and your issue resolution time may increase significantly.
+
+By enabling Pulse you elect and authorize Nutanix to electronically collect Pulse diagnostic data, including system alerts via e-mail, in accordance with terms and conditions set forth in EULA.</p>
+
+  <h3 class='disable-pulse'><input type='checkbox' name='a' style='margin-right:10px;'> Disable Pulse (Not Recommended) </h3>
+
+  <hr>
+
+  <div class='two-col fw btns'>
+    <button class='secondary previous' style='opacity:0;'>Previous</button> 
+    <button class='primary expand'> Continue</button> <button class='primary accept' style='display:none;'>  Accept</button>
+  </div>
+`;
+
+
+const pulsehd =  `
+<div class='hd'> <h3>Enhanced product support.</h3>
+
+<p>Now Nutanix support can provide a quicker, more precise response in the event of an issue. This can be enable by sending additional support information via Pulse (such as entity names). Nutanix does not collect personally identifiable information. Learn more.
+
+<div class='agreed'><input type='checkbox' style='margin-right:10px;'>Enable enhance Monitoring.</p></div>
+</div>
+`
